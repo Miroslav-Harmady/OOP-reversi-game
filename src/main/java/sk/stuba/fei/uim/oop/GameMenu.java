ae1fb9f915c75ab79c512d8c1bd95ae1b1736664
Mirko <MirkoGimi@gmail.com>
2022-04-06 22:54:46 +0200

    vytvorenie menu

--- src/main/java/sk/stuba/fei/uim/oop/GameMenu.java
+++ src/main/java/sk/stuba/fei/uim/oop/GameMenu.java
@@ -1,4 +1,24 @@
 package sk.stuba.fei.uim.oop;
 
-public class GameMenu {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameMenu extends JPanel {
+    private JButton resetButton;
+    private JSlider resizeSlider;
+    private JLabel sizeInfo;
+
+    public GameMenu(){
+        super();
+        //this.setVisible(true);
+        this.setLayout(new GridLayout(1,3));
+        this.resizeSlider = new JSlider();
+        this.sizeInfo = new JLabel();
+        this.resetButton = new JButton();
+
+        this.add(this.resizeSlider);
+        this.add(this.sizeInfo);
+        this.add(this.resetButton);
+
+    }
 }
--- src/main/java/sk/stuba/fei/uim/oop/GameWindow.java
+++ src/main/java/sk/stuba/fei/uim/oop/GameWindow.java
@@ -1,6 +1,7 @@
 package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow extends JFrame {
     private GameMenu menu;
@@ -9,8 +10,14 @@ public class GameWindow extends JFrame {
 
     public GameWindow(){
         super();
-        this.setSize(400,400);
+        this.setSize(800,800);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        this.getContentPane().setBackground(Color.white);
+        this.setLayout(new BorderLayout());
+
+        this.menu = new GameMenu();
+
+        this.add(this.menu,BorderLayout.PAGE_START);
     }
 }
